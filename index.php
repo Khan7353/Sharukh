9603cf30056918cb1c5003f705cb30b15b142749
Khan7353 <50554450+Khan7353@users.noreply.github.com>
2021-09-27 19:29:51 +0530

    Create index.php
    index file

---
+++ index.php
@@ -0,0 +1,145 @@
+<?php
+  	session_start();
+  	if(isset($_SESSION['admin'])){
+    	header('location: admin/home.php');
+  	}
+
+    if(isset($_SESSION['voter'])){
+      header('location: home.php');
+    }
+?>
+<!DOCTYPE html>
+
+<html>
+<head>
+<title>Student Union Polling System</title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+
+<link href="layout/styles/layout.css" rel="stylesheet" type="text/css" media="all">
+<!-- <link href="css/user_styles.css" rel="stylesheet" type="text/css" /> -->
+<script language="JavaScript" src="js/user.js">
+</script>
+
+</head>
+</html>
+
+
+
+
+<body id="top">
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<div class="wrapper row0" style="background-color:gold;">
+  <div id="topbar" class="hoc clear"> 
+    <!-- ################################################################################################ -->
+    <div class="fl_left">
+      <ul class="faico clear">
+        <li><a class="faicon-facebook" href="https://www.facebook.com/"><i class="fa fa-facebook"></i></a></li>
+        <li><a class="faicon-pinterest" href="https://uk.pinterest.com/"><i class="fa fa-pinterest"></i></a></li>
+        <li><a class="faicon-twitter" href="https://twitter.com/"><i class="fa fa-twitter"></i></a></li>
+        <li><a class="faicon-dribble" href="https://dribbble.com/"><i class="fa fa-dribbble"></i></a></li>
+        <li><a class="faicon-linkedin" href="https://www.linkedin.com/"><i class="fa fa-linkedin"></i></a></li>
+        <li><a class="faicon-google-plus" href="https://plus.google.com/"><i class="fa fa-google-plus"></i></a></li>
+        <li><a class="faicon-rss" href="https://www.rss.com/"><i class="fa fa-rss"></i></a></li>
+      </ul>
+    </div>
+    <div class="fl_right">
+      <ul class="nospace inline pushright">
+        <li><i class="fa fa-phone"></i> +917353733590</li>
+        <li><i class="fa fa-envelope-o"></i> maniyarnet@gmail.com </li>
+      </ul>
+    </div>
+    <!-- ################################################################################################ -->
+  </div>
+</div>
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<div class="wrapper row1">
+  <header id="header" class="hoc clear"> 
+    <!-- ################################################################################################ -->
+   
+  
+    <!-- ################################################################################################ -->
+  </header>
+</div>
+
+
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+
+<?php include 'includes/header.php'; ?>
+<body class="hold-transition login-page">
+<div class="wrapper row1">
+  <header id="header" class="hoc clear"> 
+    <!-- ################################################################################################ -->
+   
+  
+    <!-- ################################################################################################ -->
+  </header>
+</div>
+<div class="wrapper bgded overlay" style="background-image:url('images/shekar.jpg');">
+<div class="login-box">
+  	<div class="login-logo">
+  		<h2 class="font-x6 uppercase btmspace-90 underlined" style="color:white"> Student Union Voting</a></h2>
+
+		
+  	</div>
+	
+  
+  	<div class="login-box-body" style="background-color:skyblue;">
+    	<p class="login-box-msg">Sign in to start your session</p>
+
+    	<form action="login.php" method="POST">
+      		<div class="form-group has-feedback">
+        		<input type="text" class="form-control" name="voter" placeholder="Voter's ID" required>
+        		<span class="glyphicon glyphicon-user form-control-feedback"></span>
+      		</div>
+          <div class="form-group has-feedback">
+            <input type="password" class="form-control" name="password" placeholder="Password" required>
+            <span class="glyphicon glyphicon-lock form-control-feedback"></span>
+          </div>
+      		<div class="row">
+    			<div class="col-xs-4">
+          			<button type="submit" class="btn btn-primary btn-block btn-flat" name="login"><i class="fa fa-sign-in"></i> Sign In</button>
+        		</div>
+      		</div>
+    	</form>
+  	</div>
+	
+	                
+	
+	
+
+
+
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<a id="backtotop" href="#top"><i class="fa fa-chevron-up"></i></a>
+<!-- JAVASCRIPTS -->
+<script src="layout/scripts/jquery.min.js"></script>
+<script src="layout/scripts/jquery.backtotop.js"></script>
+<script src="layout/scripts/jquery.mobilemenu.js"></script>
+<!-- IE9 Placeholder Support -->
+<script src="layout/scripts/jquery.placeholder.min.js"></script>
+<!-- / IE9 Placeholder Support -->
+  	<?php
+  		if(isset($_SESSION['error'])){
+  			echo "
+  				<div class='callout callout-danger text-center mt20'>
+			  		<p>".$_SESSION['error']."</p> 
+			  	</div>
+  			";
+  			unset($_SESSION['error']);
+  		}
+  	?>
+</div>
+	
+<?php include 'includes/scripts.php' ?>
+</body>
+</html>
+
